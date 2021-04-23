eaee1d1192c54e1545972c445a10b0c1d74d2a11
Nick Lafayette <lafayetten@wit.edu>
2021-04-22 21:00:38 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,12 @@
+module desPONGcito {
+	
+	//JavaFX Library
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+	exports desPONGcito;
+}
